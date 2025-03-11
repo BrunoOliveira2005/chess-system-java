704c2637dd31e852262f8dd33a7f5e23918e0b52
Bruno <bruno.pf2005@gmail.com>
2025-03-11 15:29:33 -0300

    Chess layer and printing board

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,12 +1,14 @@
 package aplication;
-
-import boardgame.Board;
-
-public class Program {
-
-	public static void main(String[] args) {
-		
-		Board board = new Board(8,8);
-	}
-
-}
+ 
+ import chess.ChessMatch;
+ 
+ public class Program {
+ 
+ 	public static void main(String[] args) {
+ 
+ 		ChessMatch chessMatch = new ChessMatch();
+ 		UI.printBoard(chessMatch.getPieces());
+ 
+ 	}
+ 	
+ }
